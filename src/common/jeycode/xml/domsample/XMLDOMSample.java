0bce3f562a52d18da7a58c0c6a5527db2dfe5232
@JeyCode <j.code.initialize@gmail.com>
2021-05-05 21:55:15 +0200

    Se formatea el código

--- src/common/jeycode/xml/domsample/XMLDOMSample.java
+++ src/common/jeycode/xml/domsample/XMLDOMSample.java
@@ -15,62 +15,63 @@ import org.xml.sax.SAXException;
 public final class XMLDOMSample
 {
 
-						private static final File XML_FILE = new File("Employees.xml");
+      private static final File XML_FILE = new File("Employees.xml");
 
-						public static void main(String[] args) throws SAXException,IOException,ParserConfigurationException
-						{
-												// Parsear
-												var domDocument = DocumentBuilderFactory.newInstance()
-																																																				.newDocumentBuilder()
-																																																				.parse(XML_FILE);
+      public static void main(String[] args) throws SAXException,IOException,ParserConfigurationException
+      {
+            // Parsear
+            var domDocument = DocumentBuilderFactory.newInstance()
+                                                    .newDocumentBuilder()
+                                                    .parse(XML_FILE);
 
-												// Obtener elementos por etiqueta
-												var employees = domDocument.getElementsByTagName("employee");
+            // Obtener elementos por etiqueta
+            var employees = domDocument.getElementsByTagName("employee");
 
-												browseNodeLst(employees);
+            browseNodeLst(employees);
 
-						}
+      }
 
-						private static void browseNodeLst(NodeList employees)
-						{
-												var count = IntStream.range(0,employees.getLength())
-																																	.mapToObj(index-> employees.item(index))
-																																	.filter(node-> node.getNodeType() == Node.ELEMENT_NODE)
-																																	.peek(XMLDOMSample::printData)
-																																	.count();
+      private static void browseNodeLst(NodeList employees)
+      {
+            var count = IntStream.range(0,employees.getLength())
+                                 .mapToObj(index-> employees.item(index))
+                                 .filter(node-> node.getNodeType() == Node.ELEMENT_NODE)
+                                 .peek(XMLDOMSample::printData)
+                                 .count();
 
-												if (count != 0)
-												{
-																		System.out.println("Procesados " + count + " nodos");
-												}
-						}
+            if (count != 0)
+            {
+                  System.out.println("Procesados " + count + " nodos");
+            }
+      }
 
-						private static void printData(Node node)
-						{
+      private static void printData(Node node)
+      {
 
-												if (!node.getNodeName()
-																					.equals("employee"))
-												{
-																		System.out.println("Nombre nodo: " + node.getNodeName() + ", Contenido: " + node.getTextContent());
-												}
-												else
-												{
-																		System.out.println("Empleado");
-																		System.out.println();
-												}
+            if (!node.getNodeName()
+                     .equals("employee"))
+            {
+                  System.out.println("Nombre nodo: " + node.getNodeName() + ", Contenido: " + node.getTextContent());
+            }
+            else
+            {
+                  System.out.println("Empleado");
+                  System.out.println();
+            }
 
-												browseAttLst(node.getAttributes());
-												browseNodeLst(node.getChildNodes());
-						}
+            browseAttLst(node.getAttributes());
+            browseNodeLst(node.getChildNodes());
+      }
 
-						private static void browseAttLst(NamedNodeMap attributes)
-						{
-												if (attributes != null)
-												{
-																		IntStream.range(0,attributes.getLength())
-																											.mapToObj(index-> attributes.item(index))
-																											.forEach(att-> System.out.println("AttNombre: " + att.getNodeName() + " // Contenido: " + att.getNodeValue()));
-												}
-						}
+      private static void browseAttLst(NamedNodeMap attributes)
+      {
+            if (attributes != null)
+            {
+                  IntStream.range(0,attributes.getLength())
+                           .mapToObj(index-> attributes.item(index))
+                           .forEach(att-> System.out.println("AttNombre: " + att.getNodeName() + " // Contenido: "
+                                                   + att.getNodeValue()));
+            }
+      }
 
 }
